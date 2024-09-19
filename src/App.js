ba75ebd6594d752507f0f9171089187ead39f5d4
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-09-19 04:40:40 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Error from "./Pages/Errorpage/Error";
 import Home from "./Pages/Homepage/Home";
 import About from "./Pages/AboutPage/About";
 import High from "./Pages/HighRisePage/High";
@@ -11,24 +10,22 @@ import Submitted from "./Pages/Submitted/submitted";
 import Sharedpages from "./SharedPages/Sharedpages";
 import { AppProvider } from "./Components/Hooks/context";
 
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () => {
   return (
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/UP-TOWN/" element={<Sharedpages />}>
+          <Route path="/" element={<Sharedpages />}>
             <Route index element={<Home />} />
-            <Route path="/UP-TOWN/about" element={<About />} />
-            <Route path="/UP-TOWN/contactus" element={<Contact />} />
-            <Route path="/UP-TOWN/login" element={<Login />} />
-            <Route path="/UP-TOWN/signup" element={<Signup />} />
-            <Route path="/UP-TOWN/submitted" element={<Submitted />} />
-            <Route path="/UP-TOWN/highrise" element={<High />}>
+            <Route path="about" element={<About />} />
+            <Route path="contactus" element={<Contact />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="submitted" element={<Submitted />} />
+            <Route path="highrise" element={<High />}>
               <Route path="gallery:id" element={<Gallery />} />
             </Route>
-            <Route path="*" element={<Error />} />
           </Route>
         </Routes>
       </BrowserRouter>
